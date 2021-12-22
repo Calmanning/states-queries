8984a9d549d3cd7c7cbcf6f7d8711e79e208e182
Calvin Manning <cal12071@esri.com>
2021-12-22 15:54:43 -0800

    updated queryFeatures call to include a save to localStorage and update setState with that information. updated the way the dataList of cities renders, so it will be more transparent.Tried working with seperating the mapView out from the other components...that's a work in progress.

--- state-boundries.js
+++ state-boundries.js
@@ -24,75 +24,46 @@ require([
         basemap: "arcgis-topographic",
         layers: [flStateBoundaries, flCityPopulations]
     });
+    map.add(flStateBoundaries, 0)
+    map.add(flCityPopulations, 1)
 
-    const view = new MapView({
-        container: "viewDiv",
-        map: map,
-        popup: {
-            autoOpenEnabled: false,
-            dockEnabled: true,
-            dockOptions: {
-                buttonEnabled: true,
-                breakpoint: false,
-                position: "bottom-right"
-                }
-            }
-    });
-    view.map.add(flCityPopulations, 1)
-    view.map.add(flStateBoundaries, 0)
     
-
-    const stateList = ["AK","CA","CO", "CT", "DE", "ME", "MI", "MT", "NY", "NV","OH", "OR", "PA", "UT", "WA"]
-
-    const stateSelectDropdown = document.createElement("select")
-        stateSelectDropdown.setAttribute("id","stateSelector");
-        document.getElementById("selectorDiv").append(stateSelectDropdown);
+    const viewBuilder =(viewToggle) => {
+        if(viewToggle){
+            view = new MapView({
+                container: "viewDiv",
+                map: map,
+                center: [-112, 50], 
+                zoom: 4, 
+                popup: {
+                    autoOpenEnabled: false,
+                    dockEnabled: true,
+                    dockOptions: {
+                        buttonEnabled: true,
+                        breakpoint: false,
+                        position: "bottom-right"
+                    }
+                } 
+            });
+        }
+    }
     
-    const createStateDropdownList = (list) => {
-        stateSelectChoices = list.map((entry) => {
-            return `<option> ${entry} </option>`
+    let viewToggle = false
+    viewBuilder(viewToggle)
+    
+    if(viewToggle) {
+        view.on("click", ({ mapPoint }) => {
+            queryStatesEditor({ mapPoint, stateSelected: null })
         })
-        stateSelectDropdown.innerHTML = stateSelectChoices
     }
-    createStateDropdownList(stateList)
-        
-    const createCityListHeadings = (cityQueryResults) => {
-        let dataHeadings = Object.keys(cityQueryResults.features[0].attributes).map((headers) => {
-            return `<span>${headers}</span>`
-        }).join(" ")
-        
-        createCityListEntries(cityQueryResults, dataHeadings)
-    }
-    
-    const cityListData = document.createElement("div")
-          cityListData.setAttribute("id", "cityList")  
-    //come back to this later.
-    const createCityListEntries = (cityQueryResults, dataHeadings)=>{
-        let cityList = cityQueryResults.features.map((city) => {
-            let property = city.attributes
-            
-            let { [ Object.keys(property)[0] ]: town } = city.attributes
-            let { [ Object.keys(city.attributes)[1] ]: population } = city.attributes
-            console.log(town)
-            return (
-                `<p>${town}: ${population}</p>`)
-            }).join("")
-            
-       document.getElementById("dataDiv").append(cityListData)
-       cityListData.innerHTML = `<h3>${dataHeadings}</h3>${cityList}`
-    }
-
-    flCityPopulations.load().then(() => {
-        view.extent = flCityPopulations.fullExtent;
-    })
-    
-    //the visibleFieldNames here determine what is shown in the popup.
+    //the visibleFieldNames here determine what is shown in the popup. Which is currently displayed on the layerView
     flStateBoundaries.load().then(() => {
         flStateBoundaries.popupTemplate = flStateBoundaries.createPopupTemplate({
             visibleFieldNames: new Set(["STATE_NAME", "SUB_REGION", "STATE_ABBR", "POPULATION"]),
         });
     });
     
+    //this graphic get applied to the layerView
     const stateOutlineGraphic = new Graphic ({
         symbol: {
             type: "simple-fill",
@@ -103,20 +74,74 @@ require([
             }
         }
     });
-        
-    view.on("click", ({ mapPoint }) => {
-        queryStatesEditor({ mapPoint, stateSelected: null })
-        console.log(mapPoint)
+    
 
-    })
+    const mapViewBtn = document.createElement("button");
+    mapViewBtn.setAttribute("id", "mapViewBtn");
+    mapViewBtn.innerHTML = (viewToggle) ? "MapView on" : "MapView off"
+    document.getElementById("btnDiv").append(mapViewBtn);
 
+    
+    //in it's current state, almost everything the stateSelectDropdown could be it's own separate file-component.
+    const stateList = ["AK","CA","CO", "CT", "DE", "ME", "MI", "MT", "NY", "NV","OH", "OR", "PA", "UT", "WA"]
+    
+    const stateSelectDropdown = document.createElement("select")
+    stateSelectDropdown.setAttribute("id","stateSelector");
+    document.getElementById("selectorDiv").append(stateSelectDropdown);
+    
+    const createStateDropdownList = (list) => {
+        stateSelectChoices = list.map((entry) => {
+            return `<option> ${entry} </option>`
+        })
+        stateSelectDropdown.innerHTML = stateSelectChoices
+    }
+    
     stateSelectDropdown.addEventListener("change", (event) => {
-        updateQuery(event.target.value)
+        setState(event.target.value)
     });
     
+    createStateDropdownList(stateList)
+
+    const createCityListHeadings = (cityQueryResults) => {
+        let dataHeadings = Object.keys(cityQueryResults.features[0].attributes).map((headers) => {
+            return `<span>${headers}</span>`
+        }).join(" ")
+        
+        createCityListEntries(cityQueryResults, dataHeadings)
+    }
+    
+    const cityListData = document.createElement("div")
+          cityListData.setAttribute("id", "cityList")  
+
+    const createCityListEntries = (cityQueryResults, dataHeadings)=>{
+        let cityList = cityQueryResults.features.map((city) => {
+            
+            let town = city.attributes.NAME
+            let population  = city.attributes.POPULATION
+
+            return (
+                `<p>${town}: ${population}</p>`)
+            }).join("")
+            
+       document.getElementById("dataDiv").append(cityListData)
+       cityListData.innerHTML = `<h3>${dataHeadings}</h3>${cityList}`
+    }
+    
+
+        
+
+
+    
+    //this function, calling other functions would stay.
     const setState = (state) => {
-        updateStateDropdownSelector(state);  
-        filterCitiesByState(state); 
+        if(localStorage.length === 1 ) {
+            updateStateDropdownSelector(state);  
+            filterCitiesByState(state);
+            localStorage.clear() 
+        } else {
+            updateQuery(state);
+            filterCitiesByState(state); 
+        }        
     }
     
     const updateStateDropdownSelector = (state) => {
@@ -165,19 +190,32 @@ require([
     }
     
     const statesQuery = (query) => {
-        flStateBoundaries.queryFeatures(query)
-        .then((queryResults) => {
-            let stateName = queryResults.features[0].attributes.STATE_ABBR
-            setState(stateName);  
-            addStateHighlight(queryResults)
-        });
+        if(query.geometry){
+                flStateBoundaries.queryFeatures(query)
+            .then((queryResults) => {
+                let stateAbbreviation = queryResults.features[0].attributes.STATE_ABBR
+                localStorage.setItem('state', stateAbbreviation)
+                setState(stateAbbreviation)
+                addStateHighlight(queryResults)
+            });
+        } else {
+                flStateBoundaries.queryFeatures(query)
+            .then((queryResults) => {
+                addStateHighlight(queryResults)
+            });
+        }
     }
 
     const queryFilteredCities = (queryTemplate) => {
         flCityPopulations.queryFeatures(queryTemplate).
-        then((queryResults) => {
+    then((queryResults) => {
             createCityListHeadings(queryResults)
-    })
-}
+        });
+    }
+
+    
+
+
+
 
 });
