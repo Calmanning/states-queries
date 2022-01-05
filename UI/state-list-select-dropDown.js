6a54f20b4df64000361708239c323cd94d4cb6e1
Calvin Manning <cal12071@esri.com>
2022-01-05 09:11:09 -0800

    removed pre-existing state list. that originally populated the select dropdown. Sorted states populated to the list.

--- UI/state-list-select-dropDown.js
+++ UI/state-list-select-dropDown.js
@@ -1,4 +1,4 @@
-const stateList = ["AK","CA","CO", "CT", "DE", "ME", "MI", "MT", "NY", "NV","OH", "OR", "PA", "UT", "WA"]
+// const stateList = ["AK","CA","CO", "CT", "DE", "ME", "MI", "MT", "NY", "NV","OH", "OR", "PA", "UT", "WA"]
 
 const stateSelectDropdown = document.createElement("select")
     stateSelectDropdown.setAttribute("id","stateSelector");
@@ -10,6 +10,6 @@ const populateStateDropdownList = (list) => {
         return `<option> ${entry.attributes.STATE_ABBR} </option>`
     });
     
-    stateSelectDropdown.innerHTML = stateSelectChoices;
+    stateSelectDropdown.innerHTML = stateSelectChoices.sort();
 };
 
